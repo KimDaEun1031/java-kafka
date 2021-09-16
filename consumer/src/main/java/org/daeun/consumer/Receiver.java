bd11d424137062b90f8b78a3d4f39e40a95115bb
김다은 <73865700+KimDaEun1031@users.noreply.github.com>
2021-09-16 17:32:34 +0900

    Change subscribe method return type

--- consumer/src/main/java/org/daeun/consumer/Receiver.java
+++ consumer/src/main/java/org/daeun/consumer/Receiver.java
@@ -9,7 +9,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +21,7 @@ public class Receiver {
     public Receiver(String group, String topic) {
         this.consumer = new KafkaConsumer<>(receiverProps(group));
         //Topic 구독
-        consumer.subscribe(Arrays.asList(topic));
+        consumer.subscribe(Collections.singletonList(topic));
     }
 
     public void close() {
