13db2cb4bc516690a14ec54a7abfdbff1a88f532
김다은 <73865700+KimDaEun1031@users.noreply.github.com>
2021-09-16 15:52:07 +0900

    Correct parameters in SenderApplication.java Refactoring * Application.java

--- producer/src/main/java/org/daeun/producer/SenderApplication.java
+++ producer/src/main/java/org/daeun/producer/SenderApplication.java
@@ -13,21 +13,27 @@ public class SenderApplication {
 
     public static void main(String[] args) throws InterruptedException, IOException {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String topic = getTopic();
+        send(topic);
+    }
 
+    private static void send(String topic) throws InterruptedException {
         Sender sender = new Sender();
 
-        args = new String[1];
-
-        System.out.print("topic : ");
-        args[0] = bufferedReader.readLine();
-        bufferedReader.close();
-
-        for (int i = 0; i < 1000; i++) {
-            sender.sendMessage(args[0], args[0], LocalDateTime.now().toString());
+        for (int i = 0; i < 20; i++) {
+            sender.sendMessage(topic, "test" + i, LocalDateTime.now().toString());
             TimeUnit.MILLISECONDS.sleep(1000);
         }
 
         sender.close();
+    }
+
+    private static String getTopic() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("topic : ");
+        String topic = bufferedReader.readLine();
+        bufferedReader.close();
+        return topic;
     }
 }
